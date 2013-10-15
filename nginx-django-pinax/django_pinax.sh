2eeec3deda8537a4c648feac6488b3bbd0ea3d47
Ubuntu <ubuntu@ip-172-31-16-150.eu-west-1.compute.internal>
2013-10-15 19:19:42 +0000

    add installation of nginx and pinax

---
+++ nginx-django-pinax/django_pinax.sh
@@ -0,0 +1,6 @@
+mkvirtualenv scivm.com
+pip install Django==1.5.4
+django-admin.py startproject --template=https://github.com/pinax/pinax-project-account/zipball/master scivm.com
+cd scivm.com
+pip install -r requirements.txt
+python manage.py syncdb && python manage.py runserver
