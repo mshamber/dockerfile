736848478345e493dc667d0cb7a9872b99633f54
Ubuntu <ubuntu@ip-172-31-16-150.eu-west-1.compute.internal>
2013-10-15 20:36:52 +0000

    update command list

--- nginx-django-pinax/django_pinax.sh
+++ nginx-django-pinax/django_pinax.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+set -x
 python --version
-mkvirtualenv scivm.com
+virtualenv scivmcom
 pip install Django==1.5.4
-django-admin.py startproject --template=https://github.com/pinax/pinax-project-account/zipball/master scivm.com
-cd scivm.com
+django-admin.py startproject --template=https://github.com/pinax/pinax-project-account/zipball/master scivmcom2
+find scivmcom
+find scivmcom2
+cd scivmcom2
 pip install -r requirements.txt
 python manage.py syncdb && python manage.py runserver
