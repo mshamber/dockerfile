639923117e36f208db45b76e7bb7d9e39327ee43
Michael Shamberger <michal.shamberger@europa.local.eu>
2013-10-16 14:49:51 +0300

    some script fixes

--- nginx-django-pinax/django_pinax.sh
+++ nginx-django-pinax/django_pinax.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 python --version
-virtualenv scivmbase
-source ~/scivmbase/bin/activate
+mkdir -p /opt
+cd /opt
+virtualenv scivm-env
+source ~/scivm-env/bin/activate
 pip install Django==1.5.4
 django-admin.py startproject --template=https://github.com/pinax/pinax-project-account/zipball/master scivmcom
 cd scivmcom
 pip install -r requirements.txt
-python manage.py syncdb && python manage.py runserver
+#python manage.py syncdb && python manage.py runserver
